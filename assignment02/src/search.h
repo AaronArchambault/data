4b9d95db5e3cbdd04d24706f8b3fc183bbb1e62b
BigA <robert.archambault@mymail.champlain.edu>
2025-09-10 20:35:14 -0400

    adding comments
    adding comments to the code mainly in util.cpp on all the code that I added
    making sure the author has my name on them with the ones I edited

--- assignment02/src/search.h
+++ assignment02/src/search.h
@@ -4,7 +4,7 @@
 //  Functions for doing linear search and binary search on an
 //  arbitrary array.
 //  You SHOULD modify this file.
-//Aaron Archambault
+//  Aaron Archambault
 //  Copyright 2019 David Kopec
 //
 //  Permission is hereby granted, free of charge, to any person
@@ -36,24 +36,32 @@ namespace csi281 {
   // Returns the first location of the found key
   // or -1 if the key is never found
   template <typename T> int linearSearch(T array[], const int length, const T key) {
-    // YOUR CODE HERE
+    //this loops until i is grater than length/the length that was passed in and everytime it loops
+    //it checks to see if the array value of the i index is the same value as key/the thing that is
+    //being looked for and if that is true it return i but if it is not true it add 1 to the value of
+    // i until i is grater than length and if that is true and it never finds and arry i index that is
+    //the value of key it returns -1
     for (int i = 0; i < length; i++) {
       if (array[i] == key) {
         return i;
       }
-
     }
     return -1;
-
   }
 
   // Returns the first location of the found key
   // or -1 if the key is never found; assumes a sorted array
   template <typename T> int binarySearch(T array[], const int length, const T key) {
-    // YOUR CODE HERE
+    //it creates a low and high int that are set to 0 and the length passed in - 1
     int low = 0;
     int high = length - 1;
-
+//it loops while low is less than or = high and everytime it loops it create a middle int and sets it to
+    //the value of low+high / 2 to find the middle vale of the "values" then it tests if the array of middle
+    //index is the same as the value of key and if it is true then it returns middle, but if it is false it
+    //check to see if array middle index is less than the key value and if that is ture it sets the value of low
+    //to be the middle value + 1, but if it is false then it knows that array middle index is grater than key value so
+    //it sets the high value to be middle - 1 and if it can not find when array middle index = key/when the loop ends
+    //it returns -1 to show that it could not find it
     while (low <= high) {
       int middle = (low + high) / 2;
 
@@ -67,7 +75,6 @@ namespace csi281 {
         high = middle - 1;
       }
     }
-
     return -1;
   }
 
--- assignment02/src/util.cpp
+++ assignment02/src/util.cpp
@@ -5,7 +5,7 @@
 //  You SHOULD modify this file.
 //
 //  Copyright 2019 David Kopec
-//Aaron Archambault
+//  Aaron Archambault
 //  Permission is hereby granted, free of charge, to any person
 //  obtaining a copy of this software and associated documentation files
 //  (the "Software"), to deal in the Software without restriction,
@@ -40,18 +40,25 @@ namespace csi281 {
   // Returns a new int array of *length* and filled
   // with numbers between *min* and *max*
   // Suggest using the facilities in STL <random>
+
+  //pre: the randomIntArray must exist, and the length, min, and max must be passed in
+  //post: it will return the populated array that it made
+  //purpose: to generate an array and populate it with random numbers that are in the min and max
+  //ranges that are given
   int *randomIntArray(const int length, const int min, const int max) {
-    // YOUR CODE HERE
+    //creates the array that will be on the heap
     int* array = new int[length];
 
-
+    //calls and seeds the random number generator and gives it the range of the min and max values
       random_device rd;
       mt19937 gen(rd());
       uniform_int_distribution<> dist(min, max);
-
+//loops until i is greater than length, and every time it loops it generates a random number and
+    // it sets the value of the array of index i to that value and then it loops until i is greater than
+    //length make it so that every index in the array of length is populated then when it exits the
+    //loop it returns the array
     for (int i = 0; i < length; i++) {
       array[i] = dist(gen);
-
     }
     return array;
   }
@@ -71,7 +78,15 @@ namespace csi281 {
   // and stop a timer using
   // auto end = duration_cast< nanoseconds >(system_clock::now().time_since_epoch()).count();
   // start, end will be results in nanoseconds
+
+  //pre: the linear search, binary search, and int *randomIntArray have to exist and be coded right
+  // also the length and numTests must be passed in
+  //post: it will calculate the average speed of the linear search and binary search, and return
+  //the averages back as a pair
+  //purpose: to calculate the average speed of the linear search and binary search, and return
+  //the averages
   pair<nanoseconds, nanoseconds> arraySearchSpeed(const int length, const int numTests) {
+// calls the randomintarray and sets the test array and test key to that value created/gotten
     int *testArray = randomIntArray(length, 0, length);
     int *testKeys = randomIntArray(numTests, 0, length);
 
@@ -80,9 +95,13 @@ namespace csi281 {
     // Do numTests linear searches and find the average time
     // Put the result in a variable linearSearchSpeed
 
-    // YOUR CODE HERE
+    // sets the linearStart to the value of duration_cast
     auto linearStart  = duration_cast< nanoseconds >(system_clock::now().time_since_epoch()).count();
 
+    //loops until i is greater than the numTests and every time it loops it sets the result to be the
+    //value it gets from the linearSearch and its job is to run for all the num of test to see how long
+    //sorting is after i is greater than numTests it sets linerEnd to the duration_cast then it sets
+    //linearSearchSpeed to the value of linearEnd - linearStart / numTests
     for (auto i = 0; i < numTests; i++) {
     int result = linearSearch(testArray, length, testKeys[i]);
     }
@@ -91,20 +110,26 @@ namespace csi281 {
     // Do numTests binary searches and find the average time
     // Put the result in a variable binarySearchSpeed
 
+    //sorts the testarray to be in the right order so that binary search can work
     sort(testArray, testArray + length);
 
+    // sets the binaryStart to the value of duration_cast
     auto binaryStart  = duration_cast< nanoseconds >(system_clock::now().time_since_epoch()).count();
 
+    //loops until i is greater than the numTests, and every time it loops it sets the result to be the
+    //value it gets from the binarySearch and its job is to run for all the num of test to see how long
+    //sorting is after i is greater than numTests it sets binaryEnd to the duration_cast then it sets
+    //binarySearchSpeed to the value of binaryEnd - binaryStart / numTests
     for (auto i = 0; i < numTests; i++) {
       int result = binarySearch(testArray, length, testKeys[i]);
     }
 
     auto binaryEnd = duration_cast< nanoseconds >(system_clock::now().time_since_epoch()).count();
     auto binarySearchSpeed = (binaryEnd - binaryStart) / numTests;
-    // YOUR CODE HERE
+    //it then frees up the memory in the heap by deleting the test array and test keys
 
     //delete testArray;
-   // delete testKeys;
+    //delete testKeys;
     delete[] testArray;
     delete[] testKeys;
 
