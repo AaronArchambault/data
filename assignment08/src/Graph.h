d1215e13ef4120de3ac6030c593d9d12c2861c7e
BigA <robert.archambault@mymail.champlain.edu>
2025-10-27 11:21:36 -0400

    adding in assgnment 8
    working on adding assingment 8 with comments and the code

--- assignment08/src/Graph.h
+++ assignment08/src/Graph.h
@@ -6,6 +6,7 @@
 //  You should not add any additional methods to this class.
 //
 //  Copyright 2019 David Kopec
+//  Aaron Archambault
 //
 //  Permission is hereby granted, free of charge, to any person
 //  obtaining a copy of this software and associated documentation files
@@ -54,6 +55,7 @@ namespace csi281 {
     // *bidirectional* is whether the edge should be in both directions
     // If *from* and/or *to* do not exist, they are added as vertices
     void addEdge(V from, V to, bool bidirectional = true) {
+
       adjacencyList[from].emplace(to);
       if (bidirectional) {
         adjacencyList[to].emplace(from);
@@ -73,8 +75,31 @@ namespace csi281 {
 
     // Determines whether there is an edge between *from* and *to*
     // if either is not in the graph, return false
+    //pre: the graph should exist and the from and to need to be passed in
+    //post: it will test whether there is an edge between *from* and *to* and it will return something differnt depeding
+    //it will reutrn false if the the egeds don't exist it will reutrn true if to and from are connect and it will reutrn false if it can find
+    //them being connected
+    //purpose: to have a funciton that tests/determies and returns ture or false whether there is an edge between *from* and *to* and
+    //if either is not in the graph, return false
     bool edgeExists(const V &from, const V &to) {
       // YOUR CODE HERE
+
+      if (adjacencyList.find(from) == adjacencyList.end()) //it checks/test if form exists in the graph
+        //if it does not exist, there can't be an edge from it
+      {
+        return false;
+      }
+
+      for (const auto &edge : adjacencyList[from]) //loops through all edges neighbors form the 'from' vertex
+        //to check if 'to' is connected to 'from'
+        {
+        if (to == edge) //test if it finds the 'to' vertex in the adjecency list of 'from' then
+          //an edge exists between them
+          {
+          return true;
+        }
+      }
+      return false; //if all the edges are checked and edges did not find to it returns false
     }
 
     using Path = list<V>;
@@ -96,12 +121,43 @@ namespace csi281 {
     // Perform a depth-first search from *start*, looking for *goal*
     // Return a path if one is found using pathMathToPath (with explored)
     // or return nullopt if no path is found
+    //pre: the graph should exist and the stat and goal need to be passed in
+    //post: th goal/path to the goal should be found and returned as a path using pathMapToPath unsing dpeth-first search or if it can't
+    //find the goal/path to the goal then it will return nullopt
+    //purpose: to create a function that uses depth-first search to find/try and find the path to the goal that it was given using the start
+    //it was given and to return that path
     optional<Path> dfs(const V &start, const V &goal) {
       // how did we get to each node, and which ones have already been visited (dual purpose)
       unordered_map<V, V> explored = unordered_map<V, V>();
       // the start node came from nowhere, so we mark its parent as itself
       explored[start] = start;
 
+      stack<V> front; //creates the satck front that DFS can explore before backtracking
+      front.push(start); //adds the start node to the front so that DFS can start to explore
+
+      while (!front.empty()) //it loops wilhe the front is not empty so that it keeps exploring
+      //while there are still nodes in the fornt
+        {
+        V current = front.top(); //gets the top/most recoently added node to explore from the top of the stack
+        front.pop();
+
+        if (current == goal) //tests if it has reached/found the goal
+        {
+          V goalCopy = goal; //crates a copy of goal to pass to pathMapToPath
+          return pathMapToPath(explored, goalCopy); //when it is found, it converts it to an expleoted map to a path and returns it
+        }
+        for (const auto &edge : adjacencyList[current]) //it loops to explore all the neighbors edged of the current node
+          {
+          const V &to = edge;
+          if (explored.find(to) == explored.end()) //tests if the neighbor has not been explored yet
+            {
+            explored[to] = current; //marks it as explored and record that it has reached it form the curent thing/node
+            front.push(to); //adds this neighbor to the frontier for future exploraiton if needed
+          }
+        }
+      }
+      return nullopt; //if nothing has been found/reached from all possible nodes and it does not find the goal it returns null
+      //to say that no path exists
       // YOUR CODE HERE
       // TIP: Start by defining a frontier and putting start onto it.
       // TIP: Follow the pseudocode from the slides from class
@@ -110,12 +166,44 @@ namespace csi281 {
     // Perform a breadth-first search from *start*, looking for *goal*
     // Return a path if one is found using pathMathToPath (with explored)
     // or return nullopt if no path is found
+    //pre: the graph should exits and the start and goal should be passed in
+    //post: it will either return the path that it found to the goal using pathMathToPath or nullopt if it could
+    //not find the path
+    //purpose: to create a function to used breadth-first search to find the path to the goal that was passed in and starting
+    //from the start that was passed in and either return the path to the goal if it finds it or nullpot if it can't find it
     optional<Path> bfs(const V &start, const V &goal) {
       // how did we get to each node, and which ones have already been visited (dual purpose)
       unordered_map<V, V> explored = unordered_map<V, V>();
       // the start node came from nowhere, so we mark its parent as itself
       explored[start] = start;
 
+      queue<V> front; //creates the queue front so that BFS can explore level by level
+      front.push(start); //adds the start node to the front to start the exploration
+
+      while (!front.empty()) //it loops so that it can continue to explore while there are nodes in the fort
+        {
+        V current = front.front(); //gets oldest node form the front of the queue and it does this to
+        //make sure that it explores the nodes in the oder they were discoverd
+        front.pop();
+
+        if (current == goal) //tests if it has reached/found the goal
+        {
+          V goalCopy = goal;// makes a copy of the goal to pass to the pathMapToPath
+          return pathMapToPath(explored, goalCopy); //when it is found it converts it to expleoted map to a path and returns it
+        }
+
+        for (const auto &edge : adjacencyList[current]) //it loops so that it explores all of the neighbors/edges of the current node
+          {
+          const V &to = edge;
+          if (explored.find(to) == explored.end()) //checks if this node/neighbor has not been explored yet
+            {
+            explored[to] = current; //marks it as explored and records that it reached it form the curent
+            front.push(to); //adds this neighbor to the back of the queue for future exploration
+          }
+        }
+      }
+     return nullopt; //if it has done all of the reachable node without finding the goal it returns null to tell
+      //that it could not find the path
       // YOUR CODE HERE
       // TIP: Start by defining a frontier and putting start onto it.
       // TIP: Follow the pseudocode from the slides from class
